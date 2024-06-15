322c196b400058fcf9c6ea64230d68aefbfb7a18
EiNgonPwint <eingonpwint13254@gmail.com>
2024-06-15 20:28:29 +0800

    uuidv4() in product.js[Delete]

--- ENPDotNetCore.AddToCart/product.js
+++ ENPDotNetCore.AddToCart/product.js
@@ -270,12 +270,6 @@ function CalculateTotal() {
     $('#totalPrice').text(`$ ${totalPrice.toFixed(2)}`);
 }
 
-function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-    );
-}
-
 function getCart() {
     const cart = localStorage.getItem(tblCart);
     console.log(cart);
