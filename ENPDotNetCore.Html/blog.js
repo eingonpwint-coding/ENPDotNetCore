06990afe234d71a0056b937f040aad19313ed022
EiNgonPwint <eingonpwint13254@gmail.com>
2024-06-15 18:04:14 +0800

    uuidv4() in product.js [Delete]

--- ENPDotNetCore.Html/blog.js
+++ ENPDotNetCore.Html/blog.js
@@ -233,12 +233,6 @@ function deleteBlog(id) {
     );
 }
 
-function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-        (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
-    );
-}
-
 function getBlogs() {
     const blogs = localStorage.getItem(tblBlog);
     console.log(blogs);
